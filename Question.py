3bec80f04d77ab0922afe27afc3fd6e5cbd4812f
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2019-02-10 11:07:53 -0500

    Add files via upload

--- Question.py
+++ Question.py
@@ -1,17 +1,16 @@
-class question():
-    def __init__(q, c, a):
+class Question():
+    def __init__(self,q, c, a):
         self.question = q
-        self.choices = []
+        self.choices = c
         self.correct = a
-    def getQuestion():
-        return question
-    def getChoices():
-        return choices
-    def getAnswer():
-        return answer
-    def checkAnswer(choice):
-        if(choice == question.correct):
+    def getQuestion(self):
+        return self.question
+    def getChoices(self):
+        return self.choices
+    def getAnswer(self):
+        return self.answer
+    def checkAnswer(self,choice):
+        if(self.choice == question.correct):
             return true
         else:
             return false
-            
